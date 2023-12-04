0ddbbbe1244f9979d756e89f1ce16565407c2b7a
adityarizkydn <adityarizkydn@gmail.com>
2023-12-04 12:09:32 +0700

    menghilangkan tulisan Paragraf <p>

--- Javascript/TripleTen-Javascript-Song/script.js
+++ Javascript/TripleTen-Javascript-Song/script.js
@@ -4,6 +4,9 @@ let songsContainer = container.querySelector('.songs-container');
 const addButton = container.querySelector(".form__submit-btn_action_add");
 const resetButton = container.querySelector(".form__submit-btn_action_reset");
 
+let songs = songsContainer.querySelectorAll(".song");
+let noSongsElement = container.querySelector(".no-songs");
+
 function renderAdded() {
   let songs = songsContainer.querySelectorAll('.song');
 
@@ -17,16 +20,36 @@ function renderAdded() {
 }
 
 function addSong() {
-  songsContainer.innerHTML += `
+  songsContainer.insertAdjacentHTML('beforeend', `
     <div class="song">
       <h4 class="song__artist">The Cars</h4>
       <p class="song__title">Drive</p>
       <button class="song__like"></button>
     </div>
-  `;
+  `);
 
   // Call renderAdded function after a song is added
   renderAdded();
+}
+
+// Fungsi untuk merender ulang tampilan setelah lagu ditambahkan atau dihapus
+function renderAdded() {
+  // Jika tidak ada lagu dalam daftar
+  if (songs.length === 0) {
+    // Nonaktifkan tombol reset
+    resetButton.setAttribute("disabled", true);
+    // Tambahkan class untuk menandai tombol reset sebagai nonaktif
+    resetButton.classList.add("form__submit-btn_disabled");
+    // Tampilkan teks "Tidak ada lagu yang ditambahkan"
+    noSongsElement.classList.remove("no-songs_hidden"); 
+  } else {
+    // Aktifkan tombol reset
+    resetButton.removeAttribute("disabled");
+    // Hapus class yang menandai tombol reset sebagai nonaktif
+    resetButton.classList.remove("form__submit-btn_disabled");
+    // Sembunyikan teks "Tidak ada lagu yang ditambahkan"
+    noSongsElement.classList.add("no-songs_hidden"); 
+  }
 }
 
 addButton.addEventListener('click', addSong);
